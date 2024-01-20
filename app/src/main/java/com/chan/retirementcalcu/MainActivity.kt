866d2df813d16a4d7de325bbb06ce9e838da70a7
christianjj <thegreath123@gmail.com>
2024-01-20 09:52:27 +0800

    track events

--- app/src/main/java/com/chan/retirementcalcu/MainActivity.kt
+++ app/src/main/java/com/chan/retirementcalcu/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.chan.retirementcalcu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var textview: TextView
+    lateinit var interestRate: EditText
+    lateinit var age: EditText
+    lateinit var retirementAge: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button = findViewById(R.id.button)
+        textview = findViewById(R.id.textView)
+        interestRate = findViewById(R.id.interestRate)
+        age = findViewById(R.id.currentAge)
+        retirementAge = findViewById(R.id.retirementAge)
+        AppCenter.start(
+            application, "1ea0e851-abf0-4b0e-af37-16a714a91419",
+            Analytics::class.java, Crashes::class.java
+        )
+
+
+
+        button.setOnClickListener {
+            //throw Exception("something went wrong");
+           // textview.text = "tere"
+            //Crashes.generateTestCrash()
+
+            val intRate = interestRate.text.toString().toFloat()
+            val ageRate = age.text.toString().toInt()
+            val retireAge = retirementAge.text.toString().toInt()
+            if(intRate <= 0){
+                Analytics.trackEvent("wrong_interest_rate")
+            }
+            if(retireAge <= ageRate){
+                Analytics.trackEvent("wrong age")
+            }
+
+
+        }
+
     }
 }
